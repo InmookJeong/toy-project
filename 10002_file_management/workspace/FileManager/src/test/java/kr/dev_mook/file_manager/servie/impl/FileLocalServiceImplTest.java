6f9fd6ca25fd5acf588ff9365a4b8de6d23110a3
InmookJeong <jeonginmook@gmail.com>
2022-04-17 22:07:42 +0900

    test: create file or folder
    - 파일 또는 폴더 생성을 위한 테스트 코드 작성

--- 10002_file_management/workspace/FileManager/src/test/java/kr/dev_mook/file_manager/servie/impl/FileLocalServiceImplTest.java
+++ 10002_file_management/workspace/FileManager/src/test/java/kr/dev_mook/file_manager/servie/impl/FileLocalServiceImplTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 
@@ -15,7 +19,47 @@ import org.junit.Test;
 public class FileLocalServiceImplTest {
 	
 	private String samplePath = "C:\\Users\\jeong\\Desktop\\dev\\000000_workspaces\\100000_project\\10002_file_management\\workspace\\FileManager\\sample\\";
-
+	
+	/**
+	 * 파일 생성 테스트
+	 * - 같은 이름의 파일이 없는 경우에만 생성
+	 */
+	@Test
+	public void createFileTest() {
+		try {
+			Path hello2TxtPath = Paths.get(samplePath + "hello2.txt");
+			if(!hello2TxtPath.toFile().exists()) {
+				Files.createFile(hello2TxtPath);
+			}
+			
+			File file = new File(hello2TxtPath.toString());
+			assertTrue(file.exists());
+			assertTrue(file.isFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 폴더 생성 테스트
+	 * - 같은 이름의 폴더가 없는 경우에만 생성
+	 */
+	@Test
+	public void createFolderTest() {
+		try {
+			Path helloFolderPath = Paths.get(samplePath + "hello_dir");
+			if(!helloFolderPath.toFile().exists()) {
+				Files.createDirectory(helloFolderPath);
+			}
+			
+			File file = new File(helloFolderPath.toString());
+			assertTrue(file.exists());
+			assertTrue(file.isDirectory());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	/**
 	 * JUnit4에는 DisplayName이 없네...
 	 * 파일 존재 여부 확인 및 파일인지 여부 확인
@@ -33,6 +77,9 @@ public class FileLocalServiceImplTest {
 		assertFalse(isDirectory);
 	}
 	
+	/**
+	 * ㅍㄹ더 존재 여부 확인 및 폴더인지 여부 확인
+	 */
 	@Test
 	public void hasFolderTest() {
 		File folder = new File(samplePath);
